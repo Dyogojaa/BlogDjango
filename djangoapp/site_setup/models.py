49de86e94033f5c377a49e049f86fdc17308bf41
DyogoJAA <dyogojaa@gmail.com>
2024-01-21 17:14:26 -0300

    Ajuste na Visualização do Post

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-
-# Create your models here.
-
 from django.db import models
 from utils.model_validator import validate_png
 from utils.images import resize_image
+
 
 class MenuLink(models.Model):
     class Meta:
